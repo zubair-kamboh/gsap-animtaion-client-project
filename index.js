8802ee94745786fb5513ce79e35f721a68aa0a83
Zubair Ali <zubairkamboh9010@gmail.com>
2021-07-06 16:52:16 +0500

    Fixed mobile layout

--- index.js
+++ index.js
@@ -1,34 +1,143 @@
 const icon = document.querySelector('.icon-top')
 const header = document.querySelector('#header')
-const tl = new gsap.timeline()
+const navLinks = document.querySelector('#menu .menu-links')
+const navLinksLi = document.querySelectorAll('.menu-links li')
+
+const onMouseEnter = () => {
+  icon.classList.toggle('morpharrow')
+
+  navLinks.classList.add('onMenuHover')
+
+  // Animate links
+  navLinksLi.forEach((li, i) => {
+    if (li.style.animation) {
+      li.style.animation = ''
+    } else {
+      li.style.animation = `navLinkFade 0.2s ease-in forwards ${i / 7}s`
+    }
+  })
+}
+
+const onMouseLeave = () => {
+  icon.classList.toggle('morpharrow')
+
+  navLinks.classList.remove('onMenuHover')
+
+  // Animate links
+  navLinksLi.forEach((li, i) => {
+    if (li.style.animation) {
+      li.style.animation = ''
+    } else {
+      li.style.animation = `navLinkFadeOut 0.5s ease-out forwards ${
+        i / 7 + 0.2
+      }s`
+    }
+  })
+}
+
+const onMenuClick = () => {
+  const menu = document.querySelector('#menu')
+  menu.classList.toggle('onMenuClick')
+}
+
+const handleResize = (e) => {
+  if (e[0].contentRect.width < 1200) {
+    header.removeEventListener('mouseenter', onMouseEnter)
+    header.removeEventListener('mouseleave', onMouseLeave)
+
+    icon.addEventListener('click', onMenuClick)
+  } else {
+    // remove menu click listener
+    icon.removeEventListener('click', onMenuClick)
+
+    header.addEventListener('mouseenter', onMouseEnter)
+    header.addEventListener('mouseleave', onMouseLeave)
+  }
+}
+
+const resizer = new ResizeObserver(handleResize)
+resizer.observe(document.querySelector('body'))
+
+// icon.addEventListener('click', () => {
+//   navLinks.style.display = 'flex'
+// })
+
+// const handleResize = (e) => {
+//   if (e[0].contentRect.width > 733) {
+//     desktopAnimation()
+//   } else {
+//     mobileAnimation()
+//   }
+// }
+
+// const desktopAnimation = () => {
+//   header.addEventListener('mouseenter', (e) => {
+//     icon.classList.toggle('morpharrow')
+//     tl.fromTo(
+//       '.menu-links li a span',
+//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' },
+//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 }
+//     )
+//   })
+
+//   header.addEventListener('mouseleave', () => {
+//     icon.classList.toggle('morpharrow')
+//     tl.fromTo(
+//       '.menu-links li a span',
+//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 },
+//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' }
+//     )
+//   })
+// }
+
+// const mobileAnimation = () => {
+//   const menu = document.querySelector('.nav-menu')
+
+//   icon.addEventListener('click', () => {
+//     menu.classList.toggle('menuTransition')
+//     icon.classList.toggle('morpharrow')
+//   })
+// }
+
+// const resizer = new ResizeObserver(handleResize)
+// resizer.observe(document.querySelector('body'))
+
+// const menu = document.querySelector('.nav-menu')
+
+// icon.addEventListener('click', () => {
+//   menu.classList.toggle('menuTransition')
+//   icon.classList.toggle('morpharrow')
+// })
 
 // On mobile screen
-if (window.matchMedia('(max-width: 733px)').matches) {
-  const menu = document.querySelector('.nav-menu')
+// if (window.matchMedia('(max-width: 733px)').matches) {
+//   const menu = document.querySelector('.nav-menu')
 
-  icon.addEventListener('click', () => {
-    menu.classList.toggle('menuTransition')
-    icon.classList.toggle('morpharrow')
-  })
-}
+//   icon.addEventListener('click', () => {
+//     menu.classList.toggle('menuTransition')
+//     icon.classList.toggle('morpharrow')
+//   })
+// }
 
 // On desktop screen
-if (window.matchMedia('(min-width: 733px)').matches) {
-  header.addEventListener('mouseenter', () => {
-    icon.classList.toggle('morpharrow')
-    tl.fromTo(
-      '.menu-links li a span',
-      { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' },
-      { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 }
-    )
-  })
+// if (window.matchMedia('(min-width: 733px)').matches) {
+//   const icon = document.querySelector('.icon-top')
 
-  header.addEventListener('mouseleave', () => {
-    icon.classList.toggle('morpharrow')
-    tl.fromTo(
-      '.menu-links li a span',
-      { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 },
-      { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' }
-    )
-  })
-}
+//   header.addEventListener('mouseenter', () => {
+//     icon.classList.toggle('morpharrow')
+//     tl.fromTo(
+//       '.menu-links li a span',
+//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' },
+//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 }
+//     )
+//   })
+
+//   header.addEventListener('mouseleave', () => {
+//     icon.classList.toggle('morpharrow')
+//     tl.fromTo(
+//       '.menu-links li a span',
+//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 },
+//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' }
+//     )
+//   })
+// }
