247e4aebde90cd93a300a553a8de556de8e0de84
Zubair Ali <zubairkamboh9010@gmail.com>
2021-07-06 19:20:04 +0500

    Header Fix

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const onMouseEnter = () => {
   })
 }
 
+// On mouse leave
 const onMouseLeave = () => {
   icon.classList.toggle('morpharrow')
 
@@ -35,6 +36,7 @@ const onMouseLeave = () => {
   })
 }
 
+// On menu click
 const onMenuClick = () => {
   const menu = document.querySelector('#menu')
   menu.classList.toggle('onMenuClick')
@@ -42,102 +44,31 @@ const onMenuClick = () => {
 
 const handleResize = (e) => {
   if (e[0].contentRect.width < 1200) {
-    header.removeEventListener('mouseenter', onMouseEnter)
-    header.removeEventListener('mouseleave', onMouseLeave)
+    icon.removeEventListener('mouseenter', onMouseEnter)
+    navLinks.removeEventListener('mouseleave', onMouseLeave)
 
     icon.addEventListener('click', onMenuClick)
+
+    // Remove animation on document click
+    const doc = document.querySelector('body')
+    doc.addEventListener('click', (e) => {
+      const menu = document.querySelector('#menu')
+
+      const classClick = e.target.classList
+
+      if (classClick.contains('nav-menu')) {
+        menu.classList.remove('onMenuClick')
+      }
+    })
   } else {
     // remove menu click listener
     icon.removeEventListener('click', onMenuClick)
 
-    header.addEventListener('mouseenter', onMouseEnter)
-    header.addEventListener('mouseleave', onMouseLeave)
+    // add mouseenter & mouseleave
+    icon.addEventListener('mouseenter', onMouseEnter)
+    navLinks.addEventListener('mouseleave', onMouseLeave)
   }
 }
 
 const resizer = new ResizeObserver(handleResize)
 resizer.observe(document.querySelector('body'))
-
-// icon.addEventListener('click', () => {
-//   navLinks.style.display = 'flex'
-// })
-
-// const handleResize = (e) => {
-//   if (e[0].contentRect.width > 733) {
-//     desktopAnimation()
-//   } else {
-//     mobileAnimation()
-//   }
-// }
-
-// const desktopAnimation = () => {
-//   header.addEventListener('mouseenter', (e) => {
-//     icon.classList.toggle('morpharrow')
-//     tl.fromTo(
-//       '.menu-links li a span',
-//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' },
-//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 }
-//     )
-//   })
-
-//   header.addEventListener('mouseleave', () => {
-//     icon.classList.toggle('morpharrow')
-//     tl.fromTo(
-//       '.menu-links li a span',
-//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 },
-//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' }
-//     )
-//   })
-// }
-
-// const mobileAnimation = () => {
-//   const menu = document.querySelector('.nav-menu')
-
-//   icon.addEventListener('click', () => {
-//     menu.classList.toggle('menuTransition')
-//     icon.classList.toggle('morpharrow')
-//   })
-// }
-
-// const resizer = new ResizeObserver(handleResize)
-// resizer.observe(document.querySelector('body'))
-
-// const menu = document.querySelector('.nav-menu')
-
-// icon.addEventListener('click', () => {
-//   menu.classList.toggle('menuTransition')
-//   icon.classList.toggle('morpharrow')
-// })
-
-// On mobile screen
-// if (window.matchMedia('(max-width: 733px)').matches) {
-//   const menu = document.querySelector('.nav-menu')
-
-//   icon.addEventListener('click', () => {
-//     menu.classList.toggle('menuTransition')
-//     icon.classList.toggle('morpharrow')
-//   })
-// }
-
-// On desktop screen
-// if (window.matchMedia('(min-width: 733px)').matches) {
-//   const icon = document.querySelector('.icon-top')
-
-//   header.addEventListener('mouseenter', () => {
-//     icon.classList.toggle('morpharrow')
-//     tl.fromTo(
-//       '.menu-links li a span',
-//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' },
-//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 }
-//     )
-//   })
-
-//   header.addEventListener('mouseleave', () => {
-//     icon.classList.toggle('morpharrow')
-//     tl.fromTo(
-//       '.menu-links li a span',
-//       { x: 35, opacity: 1, ease: 'sine.inOut', duration: 0.6 },
-//       { x: -100, opacity: 0, duration: 0.6, ease: 'sine.inOut' }
-//     )
-//   })
-// }
